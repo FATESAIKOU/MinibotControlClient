a663b7898dd34458bbb56c152da9ad39be617dc9
FATESAIKOU <qzsecftbhhhh@gmail.com>
2018-06-21 14:16:36 +0800

    Complete RobotAgent

--- src/Agents/RobotAgent.py
+++ src/Agents/RobotAgent.py
@@ -5,10 +5,51 @@ An simple agent to instantiate Minibot(Third).
 @date  : 06/21/2018
 """
 
+import requests
+import json
+
 class RobotAgent:
     def __init__( self, api_url ):
-        self.__api_url = api_url;
+        self.SetUrl(api_url)
 
+    """ Initialization """
     def GetUrl( self ):
         return self.__api_url;
 
+    def SetUrl( self, api_url ):
+        try:
+            robot_resp = requests.get( api_url )
+            self.CheckResponse(robot_resp)
+
+            self.__api_url = api_url
+            self.__robot_status = json.loads(robot_resp.content)
+        except Exception as e:
+            print('Unable to set url as "' + api_url + '"\n' + str(e))
+
+
+    """ Get Status """
+    def GetStatus( self, fresh = False ):
+        if fresh or not hasattr(self, '__robot_status'):
+            robot_resp = requests.get( self.__api_url )
+            self.CheckResponse(robot_resp)
+
+            self.__robot_status = json.loads(robot_resp.content)
+        
+        return self.__robot_status
+
+
+    """ Exec """
+    def Exec( self, action, *args ):
+        robot_resp = requests.put(self.__api_url, json=['Robot', action, args])
+        self.CheckResponse(robot_resp)
+
+        self.__robot_status = json.loads(robot_resp.content)
+
+        return self.__robot_status
+        
+
+
+    """ Utils """
+    def CheckResponse( self, response ):
+        if (response.status_code != 200):
+            raise SystemError('Server responses with bad status code: ' + str(response.status_code))
