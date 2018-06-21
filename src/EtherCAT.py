d9e99492a8a6891bf5c01ee8ea345d5881385661
FATESAIKOU <qzsecftbhhhh@gmail.com>
2018-06-21 15:27:34 +0800

    Complete EtherCAT

--- src/EtherCAT.py
+++ src/EtherCAT.py
@@ -5,4 +5,24 @@ An simple agent to structurize EtherCAT net.
 @date  : 06/21/2018
 """
 
-from RobotAgent import RobotAgent
+from Agents.RobotAgent import RobotAgent
+
+class EtherCAT:
+    def __init__( self, base_url ):
+        self.__base_url = base_url
+        """ Need some check """
+
+        self.__robot_dict = {}
+
+
+    def GetRobot( self, robot_id ):
+        if robot_id not in self.__robot_dict.keys():
+            robot_url = self.__base_url + '/robot/' + str(robot_id)
+            robot_agent = RobotAgent(robot_url)
+
+            if not robot_agent.is_constructed:
+                return None
+            
+            self.__robot_dict[robot_id] = robot_agent
+
+        return self.__robot_dict[robot_id]
